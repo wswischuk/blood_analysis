5d0f2c2601dc108075a3e679b35117e6d008f7ce
Nicolas Swischuk <nws10@duke.edu>
2020-01-24 12:41:30 -0500

    Added the input for the cholesterol level analysis

--- chol_analysis.py
+++ chol_analysis.py
@@ -1,13 +1,23 @@
+def HDL_analysis(HDL_level):
+    if HDL_level >= 60:
+        return "Normal"
+    elif 40 <= HDL_level < 60:
+        return "Borderline low"
+    else:
+        return "Low"
+
+
 def cholesterol_analysis():
     print("Cholesterol Analysis")
     HDLinput = input("Enter test result: ")
     test_info = HDLinput.split("=")
     if test_info[0] == "HDL":
-        HDL_analysis()
+        answer = HDL_analysis(int(test_info[1]))
+        print("The level is {}".format(answer))
 
 def interface():
     choice = 0
-    while true:
+    while True:
         print("Cholesterol Calc")
         print("Options")
         print("  1 - Cholesterol Analysis")
