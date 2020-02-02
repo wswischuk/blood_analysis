7d93d573207b269ed5a8f2a5279cd13fa23dbd5b
Nicolas Swischuk <nws10@duke.edu>
2020-02-02 16:09:18 -0500

    Added LDL and HDL tests

--- chol_analysis.py
+++ chol_analysis.py
@@ -5,6 +5,8 @@ def HDL_analysis(HDL_level):
         return "Borderline low"
     else:
         return "Low"
+
+
 def LDL_analysis(LDL_level):
     if LDL_level >= 190:
         return "Very High"
@@ -15,12 +17,11 @@ def LDL_analysis(LDL_level):
     else:
         return "Normal"
 
+
 def cholesterol_analysis():
     print("Cholesterol Analysis")
     typetest = input("Enter test result: ")
     test_info = typetest.split("=")
-    #print(testinfo[0].strip(" "))
-    #print(test_info[1].strip(" "))
     if test_info[0] == "HDL":
         answer = HDL_analysis(int(test_info[1]))
         print("The level is {}".format(answer))
@@ -28,13 +29,16 @@ def cholesterol_analysis():
         answer = LDL_analysis(int(test_info[1]))
         print("The level is {}".format(answer))
 
+
 def new_feature():
     pass
+
 
 def name_function():
     first_name = input("Enter first name: ")
     last_name = input("Enter last name: ")
-    full_name = [first name, last name]
+    full_name = [first_name, last_name]
+
 
 def interface():
     while True:
@@ -47,6 +51,7 @@ def interface():
             return
         elif choice == "1":
             cholesterol_analysis()
+
 
 if __name__ == "__main__":
     interface()
